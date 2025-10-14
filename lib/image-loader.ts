6857fec5e519254c528807a57eefe0466561e7bb
khoile <faber8164@gmail.com>
2025-10-15 00:04:59 +0700

    correct logic of image loader

--- lib/image-loader.ts
+++ lib/image-loader.ts
@@ -2,7 +2,7 @@ export default function imageLoader({ src }: { src: string }) {
 	const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ""
 
 	// Only prepend basePath for local images (not external URLs)
-	if (src.startsWith("http")) {
+	if (!src.startsWith("http")) {
 		src = `${basePath}${src.startsWith("/") ? src : "/" + src}`
 	}
 
