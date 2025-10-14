2dd7ec1489ba941852f3c07a5dfec913c681acd7
khoile <faber8164@gmail.com>
2025-10-14 21:51:52 +0700

    add base path for nextjs workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next"
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ""
+const assetPrefix = process.env.NEXT_PUBLIC_ASSET_PREFIX || ""
+
 const nextConfig: NextConfig = {
 	output: "export",
+	basePath,
+	assetPrefix,
 	images: {
 		remotePatterns: [
 			{
