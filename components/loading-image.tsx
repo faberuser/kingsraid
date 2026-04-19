12e033b51a466a054ccf7eb34dcbab2a86c522a9
khoile <faber8164@gmail.com>
2026-04-19 19:58:00 +0700

    add loading img component

---
+++ components/loading-image.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+import Image from "next/image"
+import { useReducer } from "react"
+
+/**
+ * This component is a drop-in replacement for next/image that shows a placeholder image while the image is loading.
+ *
+ * *GitHub Issue: Placeholder of next/image is still visible after the image is loaded*\
+ * https://github.com/vercel/next.js/issues/53329
+ */
+
+export const LoadingImage: React.FC<LoadingImageProps> = ({ placeholder, ...props }) => {
+	const [isLoading, stopLoading] = useReducer((_, ev) => (props.onLoad?.(ev), false), true)
+
+	// prettier-ignore
+	return (
+    <>
+      {isLoading && placeholder &&
+        <Image
+          {...props}
+          placeholder={undefined}
+          src={placeholder}
+        />
+      }
+      <Image
+        {...props}
+        className={`${props.className} ${isLoading && 'absolute invisible'}`}
+        placeholder={undefined}
+        onLoad={stopLoading}
+      />
+    </>
+  )
+}
+
+export type NextImageProps = Parameters<typeof Image>[0]
+
+export interface LoadingImageProps extends NextImageProps {
+	placeholder: NextImageProps["placeholder"]
+}
